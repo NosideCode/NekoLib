ab20d3e35080e4f9162b04a36e164f0481c4bcc8
Noside <44101720+NosideCode@users.noreply.github.com>
2021-03-20 14:59:09 -0600

    Add missing tests

---
+++ tests/unit/Collections/QueueTest.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+namespace NekoLib\Tests\Unit\Collections;
+
+use NekoLib\Collections\Queue;
+use PHPUnit\Framework\TestCase;
+
+final class QueueTest extends TestCase
+{
+    public function testEmpty(): Queue
+    {
+        $queue = new Queue();
+        $this->assertSame(0, $queue->count());
+        $this->assertTrue($queue->isEmpty());
+        return $queue;
+    }
+
+    /**
+     * @depends testEmpty
+     */
+    public function testEnqueue(Queue $queue): Queue
+    {
+        $value = 'Pekora';
+        $queue->enqueue($value);
+        $this->assertSame($value, $queue->peek());
+        $this->assertSame(1, $queue->count());
+        $this->assertFalse($queue->isEmpty());
+        return $queue;
+    }
+
+    /**
+     * @depends testEnqueue
+     */
+    public function testDequeue(Queue $queue): void
+    {
+        $this->assertSame('Pekora', $queue->dequeue());
+        $this->assertSame(0, $queue->count());
+        $this->assertTrue($queue->isEmpty());
+    }
+
+    public function testTryPeek_FailsWhenEmpty(): void
+    {
+        $queue = new Queue();
+        $this->assertFalse($queue->tryPeek($result));
+        $this->assertNull($result);
+    }
+
+    public function testTryDequeue_FailsWhenEmpty(): void
+    {
+        $queue = new Queue();
+        $this->assertFalse($queue->tryDequeue($result));
+        $this->assertNull($result);
+    }
+}
---
+++ tests/unit/Collections/StackTest.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+namespace NekoLib\Tests\Unit\Collections;
+
+use NekoLib\Collections\Stack;
+use PHPUnit\Framework\TestCase;
+
+final class StackTest extends TestCase
+{
+    public function testEmpty(): Stack
+    {
+        $stack = new Stack();
+        $this->assertSame(0, $stack->count());
+        $this->assertTrue($stack->isEmpty());
+        return $stack;
+    }
+
+    /**
+     * @depends testEmpty
+     */
+    public function testPush(Stack $stack): Stack
+    {
+        $value = 'Watame';
+        $stack->push($value);
+        $this->assertSame($value, $stack->peek());
+        $this->assertSame(1, $stack->count());
+        $this->assertFalse($stack->isEmpty());
+        return $stack;
+    }
+
+    /**
+     * @depends testPush
+     */
+    public function testPop(Stack $stack): void
+    {
+        $this->assertSame('Watame', $stack->pop());
+        $this->assertSame(0, $stack->count());
+        $this->assertTrue($stack->isEmpty());
+    }
+
+    public function testTryPeek_FailsWhenEmpty(): void
+    {
+        $stack = new Stack();
+        $this->assertFalse($stack->tryPeek($result));
+        $this->assertNull($result);
+    }
+
+    public function testTryPop_FilsWhenEmpty(): void
+    {
+        $stack = new Stack();
+        $this->assertFalse($stack->tryPop($result));
+        $this->assertNull($result);
+    }
+}

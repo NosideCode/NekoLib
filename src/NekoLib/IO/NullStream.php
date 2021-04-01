33f6ea2776f52ca286b7b207142ad8c09571e4e3
Noside <44101720+NosideCode@users.noreply.github.com>
2021-03-31 20:16:08 -0600

    Add NullStream class

---
+++ src/NekoLib/IO/NullStream.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+namespace NekoLib\IO;
+
+/**
+ * A stream that does nothing.
+ */
+final class NullStream extends Stream
+{
+    public function isReadable(): bool
+    {
+        return true;
+    }
+
+    public function isWritable(): bool
+    {
+        return true;
+    }
+
+    public function endOfStream(): bool
+    {
+        return true;
+    }
+
+    public function getSize(): int
+    {
+        return 0;
+    }
+
+    public function setSize(int $size): void
+    {
+    }
+
+    public function getPosition(): int
+    {
+        return 0;
+    }
+
+    public function setPosition(int $position): void
+    {
+    }
+
+    public function seek(int $offset, int $whence): void
+    {
+    }
+
+    public function read(int $length, ?string &$data): int
+    {
+        return 0;
+    }
+
+    public function write(string $data, int $length = -1): int
+    {
+        return 0;
+    }
+
+    public function flush(): void
+    {
+    }
+
+    public function close(): void
+    {
+    }
+}

46285f3f0f5fb50cfa27e9c2a9786fbcc8d81839
Noside <44101720+NosideCode@users.noreply.github.com>
2021-04-26 01:06:17 -0500

    Add test for Dictionary

---
+++ tests/unit/Collections/DictionaryTest.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+namespace NekoLib\Tests\Unit\Collections;
+
+use InvalidArgumentException;
+use NekoLib\Collections\Dictionary;
+use NekoLib\Collections\KeyNotFoundException;
+use PHPUnit\Framework\TestCase;
+
+final class DictionaryTest extends TestCase
+{
+    public function testEmpty(): Dictionary
+    {
+        $map = new Dictionary();
+        $this->assertTrue($map->isEmpty());
+        $this->assertEquals(0, $map->count());
+        return $map;
+    }
+
+    /**
+     * @depends testEmpty
+     */
+    public function testAdd(Dictionary $map): Dictionary
+    {
+        $map->add('foo', 'bar');
+        $this->assertFalse($map->isEmpty());
+        $this->assertEquals(1, $map->count());
+        $this->assertEquals('bar', $map->get('foo'));
+        return $map;
+    }
+
+    /**
+     * @depends testAdd
+     */
+    public function testAddThrowsExceptionIfTheKeyExists(Dictionary $map): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $map->add('foo', 'baz');
+    }
+
+    /**
+     * @depends testAdd
+     */
+    public function testSet(Dictionary $map): Dictionary
+    {
+        // Update key
+        $map->set('foo', 'baz');
+        $this->assertEquals('baz', $map->get('foo'));
+
+        $map->set('wakipai', 'Watame');
+        $this->assertEquals('Watame', $map->get('wakipai'));
+        $this->assertEquals(2, $map->count());
+        return $map;
+    }
+
+    /**
+     * @depends testSet
+     */
+    public function testGetThrowsExceptionIfTheKeyIsNotFound(Dictionary $map): void
+    {
+        $this->expectException(KeyNotFoundException::class);
+        $map->get('unknown key');
+    }
+}
